7c0b6f3abee1306e6ab9af540a0eab74f99c999d
sylesh119 <syleshrockzz@gmail.com>
2025-11-10 10:14:03 +0530

    Updated method names and xpath's

---
+++ src/test/java/PageObjects/AccountRegistrationPage.java
@@ -0,0 +1,32 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountRegistrationPage extends BasePage {
+
+	public AccountRegistrationPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(xpath = "//input[@id='input-firstname']")
+	WebElement txtFirstname;
+
+	@FindBy(xpath = "input[@id='input-lastname']")
+	WebElement txtLastname;
+
+	@FindBy(xpath = "//input[@id='input-email']")
+	WebElement txtEmail;
+
+	@FindBy(xpath = "//input[@id='input-password']")
+	WebElement txtPassword;
+
+	@FindBy(xpath = "button[normalize-space()='Continue']")
+	WebElement btnContinue;
+
+	public void setFirstname(String fname) {
+		txtFirstname.sendKeys(fname);
+	}
+
+}
