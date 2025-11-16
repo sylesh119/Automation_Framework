4856d8b0b0c5c84dae79b51b30b7aa675c3e09b3
sylesh119 <syleshrockzz@gmail.com>
2025-11-16 21:40:50 +0530

    COMMIT

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -1,6 +1,11 @@
 package TestBase;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,10 +21,16 @@ public class BaseClass {
 
 	public WebDriver driver;
 	public Logger logger;
+	public Properties prop;
 
 	@BeforeClass
 	@Parameters({ "os", "browser" })
-	public void setup(String os, String br) {
+	public void setup(String os, String br) throws IOException {
+		
+		FileReader file = new FileReader("./src//test//resources//config.properties");
+		prop=new Properties();
+		prop.load(file);
+		
 		logger = LogManager.getLogger(this.getClass());
 		switch (br.toLowerCase()) {
 		case "chrome":
@@ -38,7 +49,7 @@ public class BaseClass {
 
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
+		driver.get(prop.getProperty("appURL"));  // READING URL FROM PROPERTIES FILE
 		driver.manage().window().maximize();
 	}
 
