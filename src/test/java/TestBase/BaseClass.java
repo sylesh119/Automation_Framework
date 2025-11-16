4713104487548c2eef73b7079ab178406cbfab5f
sylesh119 <syleshrockzz@gmail.com>
2025-11-16 21:19:09 +0530

    new

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -6,8 +6,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 
@@ -15,10 +18,24 @@ public class BaseClass {
 	public Logger logger;
 
 	@BeforeClass
-	
-	public void setup() {
+	@Parameters({ "os", "browser" })
+	public void setup(String os, String br) {
 		logger = LogManager.getLogger(this.getClass());
-		driver = new ChromeDriver();
+		switch (br.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		default:
+			System.out.println("Invalid browser");
+			return;
+		}
+
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
