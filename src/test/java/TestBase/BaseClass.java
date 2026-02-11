1c62bd3b0444064c37b8e96c4aee14c46546814e
sylesh119 <syleshrockzz@gmail.com>
2026-02-11 11:42:03 +0530

    new

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -1,14 +1,19 @@
 package TestBase;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -72,6 +77,22 @@ public class BaseClass {
 		String generatedString = RandomStringUtils.randomAlphabetic(4);
 		String generatedNumber = RandomStringUtils.randomNumeric(3);
 		return (generatedString + generatedNumber);
+	}
+
+	public String captureScreen(String tname) throws IOException {
+
+		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+				
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		
+		String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png";
+		File targetFile=new File(targetFilePath);
+		
+		sourceFile.renameTo(targetFile);
+			
+		return targetFilePath;
+
 	}
 
 }
